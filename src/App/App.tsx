5ebc102b31469b93aff733ae6aca81731b70a76d
Johnny <jl2672@cornell.edu>
2020-06-09 19:33:10 -0700

    delay loading

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import SortingMain from './SortingMain';
 
-const App: React.FC = (): JSX.Element => (
-  <div>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>Sorting Visualizer</title>
-    </Helmet>
-    <SortingMain />
-  </div>
-);
+const App: React.FC = (): JSX.Element => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 75);
+  }, []);
+
+  const preloadComponent = (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Sorting Visualizer</title>
+      </Helmet>
+    </div>
+  );
+  const loadedComponent = (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Sorting Visualizer</title>
+      </Helmet>
+      <SortingMain />
+    </div>
+  );
+  return loaded ? loadedComponent : preloadComponent;
+};
 
 export default App;
